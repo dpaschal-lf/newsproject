3c3c39cdf29d92c5662c45ad9f690a81c2260953
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-28 12:50:09 -0800

    added initial files for news app project and css

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './App.css';
+import NewsArticle from './components/NewsArticle/NewsArticle.js';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.markArticle = this.markArticle.bind( this );
+    this.getArticleStatus = this.getArticleStatus.bind( this );
+    this.state = {
+      data : []
+    }
+    this.prepStorage();
+  }
+  prepStorage(){
+    if(localStorage.articles === undefined ){
+      localStorage.articles = JSON.stringify({});
+    }
+  }
+  markArticle( articleURL ){
+    const articleData = JSON.parse( localStorage.articles );
+    if(!articleData.hasOwnProperty( articleURL )){
+      articleData[ articleURL ] = {
+        read: Date.now(),
+      }
+    }
+    localStorage.articles = JSON.stringify( articleData );
+  }
+  getArticleStatus( articleURL ){
+    const articleData = JSON.parse( localStorage.articles );    
+    return articleData.hasOwnProperty( articleURL );
+  }
+  componentDidMount(){
+    console.log('firing');
+    var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=5c8e21ff646c40f8af7f340cf4281975';
+    var req = new Request(url);
+    fetch(req)
+        .then( response => response.json() )
+        .then( response => 
+          this.setState( {
+            data: response.articles
+          })
+        );
+  }
+  makeArticleColumn( data ){
+    return (
+      <div className="articleColumn">
+        {data.map( articleData => (<NewsArticle key={articleData.url} data={articleData} markCallback={this.markArticle} checkCallback={this.getArticleStatus}/>))}
+      </div>
+    );
+  }
+  render(){
+    const data =this.state.data;
+    const columnPoints = Math.floor(data.length/3);
+    const dataDividedByThirds = [
+      this.state.data.slice(0, columnPoints+1),
+      this.state.data.slice(columnPoints+1, columnPoints*2+1),
+      this.state.data.slice(columnPoints*2+1)
+    ];
+    return(
+      <div className="articleContainer">
+        { dataDividedByThirds.map( this.makeArticleColumn )}
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/NewsArticle/NewsArticle.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './newsarticle.css';
+
+class NewsArticle extends React.Component{
+    constructor( props ){
+        super( props );
+        this.hideModal = this.hideModal.bind( this );
+        this.showModal = this.showModal.bind( this );
+        this.state = {
+            modalShowing: false,
+            viewed: false
+        }
+    }
+    generateModal(){
+        return (
+            <div className="modalShadow">
+                <div className="modalBody">
+                    <div className="modalClose" onClick={this.hideModal}>X</div>
+                    <div className="modalContent">
+                        <a target="_blank" href={this.props.data.url} rel="noopener noreferrer">
+                        <img alt={this.props.data.title} className="modalImage" src={this.props.data.urlToImage}/>
+                        { this.props.data.content}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    hideModal(){
+        this.setState({
+            modalShowing: false
+        })
+    }
+    showModal(){
+        this.setState({
+            viewed: true,
+            modalShowing: true
+        })
+    }
+    render(){
+        console.log(this.props.data);
+        let {title, urlToImage: imageURL} = this.props.data;
+        const style = {
+            'background-image' : `url(${imageURL})`
+        }
+        return(
+            <div className="article" style={ style }>
+                <div className="articleText" onClick={this.showModal}>{title}</div>
+                { this.state.modalShowing ? this.generateModal() : null}
+                { this.state.viewed ? <i className="viewedIcon far fa-eye"></i> : null }
+            </div>
+        );
+    }
+}
+
+export default NewsArticle;
